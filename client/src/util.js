88e6892678db885c0293ded57442cd957e9b78b7
Etienne Colaitis <ecolaitis@gmail.com>
2024-07-07 20:20:38 +0200

    listpeerchannels, fix undefined peer

--- client/src/util.js
+++ client/src/util.js
@@ -24,7 +24,7 @@ export const recvAmt = ({ amount_msat: expected, amount_received_msat: actual })
   (expected && (actual-expected)/expected<0.005) ? expected : actual
 
 // Parse "listpeerchannels" to get all channels as a list of (peer,channel) tuples
-export const getChannels = channels => [].concat(...channels.map(chan => ({ peer, chan })))
+export const getChannels = channels => [].concat(...channels.map(chan => ({ chan })))
 
 // TODO: remove this function and refactor the code that was using it...
 // this function is not needed anymore, as a quickfix, it is now Identity
